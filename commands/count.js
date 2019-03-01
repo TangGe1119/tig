ba637fe61627d6196797192a8fb0681874fd7f10
tyuan <tyuan@yuansuan.cn>
2019-03-01 14:09:14 +0800

    bugfix

--- commands/count.js
+++ commands/count.js
@@ -9,7 +9,7 @@ const log = console.log
 const getCommand = data =>
   `git log --author="${data.gituser}" --since=${data.start} --until=${
     data.end
-  } --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "%s-%s-%s", add, subs, loc }' -`
+  } --pretty=tformat: --numstat | awk '{ add += $1; subs += $2;} END { printf "%s-%s", add, subs }' -`
 
 module.exports = function() {
   inquirer
@@ -35,10 +35,10 @@ module.exports = function() {
     ])
     .then(answers => {
       const data = execSync(getCommand(answers), { encoding: 'utf8' })
-      const [add, remove, total] = data.trim().split('-')
+      const [add, remove] = data.trim().split('-')
       log(chalk.blue('新增') + ': ' + add)
       log(chalk.red('删除') + ': ' + remove)
-      log(chalk.green('总计') + ': ' + total)
+      log(chalk.green('总计') + ': ' + (add - remove))
     })
 }
 
